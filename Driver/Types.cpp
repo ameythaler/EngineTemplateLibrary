b301f0fced9d8391894c3c9ecc3854d46716b413
amanda <amandameythaler@gmail.com>
2017-04-21 14:12:15 -0700

    Added another string test.

--- Driver/Types.cpp
+++ Driver/Types.cpp
@@ -22,4 +22,20 @@ SCENARIO("wide and multi-byte strings can be converted back and forth", "[string
 			}
 		}
 	}
+
+	GIVEN("A multi-byte string with some text")
+	{
+		MbString mbString = _ETL_TEST_STRING;
+
+		WHEN("the multi-byte string is converted to a wide string")
+		{
+			WideString wideStr;
+			MbStringToWideString(wideStr, mbString);
+
+			THEN("the wide string matches the multi-byte string")
+			{
+				REQUIRE(wcscmp(wideStr.c_str(), ETL_TEXT(_ETL_TEST_STRING)) == 0);
+			}
+		}
+	}
 }
